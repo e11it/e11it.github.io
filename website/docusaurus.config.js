8d5640789351150a6ef19efadce0c479a08d0b83
e11it <im@e11it.ru>
2022-05-16 18:18:13 +0300

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://e11it.github.io/',
-  baseUrl: 'im/',
+  baseUrl: '/im/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
