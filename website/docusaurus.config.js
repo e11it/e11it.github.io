463558bc56f8d888720fe415565e1c5e72fccbde
e11it <im@e11it.ru>
2022-05-24 19:44:08 +0300

    Fix url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'e11it home',
   tagline: 'e11it home are cool',
   url: 'https://e11it.github.io',
-  baseUrl,
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
